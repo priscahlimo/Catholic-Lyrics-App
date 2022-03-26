4f88faaa707595e760666a38d8343f658675278f
priscahlimo <priscahlimoh@gmail.com>
2022-03-26 13:04:51 +0300

    add butterknife dependency and its implementation

--- app/src/main/java/com/priscah/catholicsongs/MainActivity.java
+++ app/src/main/java/com/priscah/catholicsongs/MainActivity.java
@@ -3,12 +3,17 @@ package com.priscah.catholicsongs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.button)
+    Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mButton.setOnClickListener(this);
     }
 }
