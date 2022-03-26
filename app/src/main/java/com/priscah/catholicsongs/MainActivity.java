f51370960d188acb2f702b061693d6a6c6735eda
priscahlimo <priscahlimoh@gmail.com>
2022-03-26 13:23:15 +0300

    add an intent that takes us to songs list

--- app/src/main/java/com/priscah/catholicsongs/MainActivity.java
+++ app/src/main/java/com/priscah/catholicsongs/MainActivity.java
@@ -2,20 +2,32 @@ package com.priscah.catholicsongs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-    @BindView(R.id.button) Button mButton;
+    private Button mFindSongsButton;
+//    @BindView(R.id.button) Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
-        mButton.setOnClickListener(this);
+
+        mFindSongsButton = (Button) findViewById(R.id.button);
+        mFindSongsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SongsList.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
--- app/src/main/java/com/priscah/catholicsongs/SongsList.java
+++ app/src/main/java/com/priscah/catholicsongs/SongsList.java
@@ -2,7 +2,9 @@ package com.priscah.catholicsongs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SongsList extends AppCompatActivity {
 
@@ -11,4 +13,5 @@ public class SongsList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_songs_list);
     }
+
 }
