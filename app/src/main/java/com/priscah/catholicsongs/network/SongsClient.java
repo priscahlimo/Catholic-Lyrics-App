d7019c8a14554e0c0d089c95e7253ac3cdee3994
priscahlimo <priscahlimoh@gmail.com>
2022-04-05 22:25:16 +0300

    create and update songs client

---
+++ app/src/main/java/com/priscah/catholicsongs/network/SongsClient.java
@@ -0,0 +1,42 @@
+package com.priscah.catholicsongs.network;
+
+import static com.priscah.catholicsongs.constants.YELP_BASE_URL;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class SongsClient {
+
+    private static Retrofit retrofit = null;
+
+    public static SongsAPI getClient() {
+
+        if (retrofit == null) {
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .addInterceptor(new Interceptor() {
+                        @Override
+                        public Response intercept(Chain chain) throws IOException {
+                            Request newRequest  = chain.request().newBuilder()
+//                                    .addHeader("Authorization", YELP_API_KEY)
+                                    .build();
+                            return chain.proceed(newRequest);
+                        }
+                    })
+                    .build();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(YELP_BASE_URL)
+                    .client(okHttpClient)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+        return retrofit.create(SongsAPI.class);
+    }
+}
