04663650e9e372de98b6b9f26bd4bdee8c7d72c0
priscahlimo <priscahlimoh@gmail.com>
2022-04-12 08:52:29 +0300

    complete sign up process

--- app/src/main/java/com/priscah/catholicsongs/ui/LoginActivity.java
+++ app/src/main/java/com/priscah/catholicsongs/ui/LoginActivity.java
@@ -1,38 +1,50 @@
 package com.priscah.catholicsongs.ui;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.priscah.catholicsongs.R;
+
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
-    @BindView(R.id.registerTextView)  TextView mRegisterTextView;
+    @BindView(R.id.loginButton)
+    Button mLoginButton;
+    @BindView(R.id.emailEditText)
+    EditText mEmailEditText;
+    @BindView(R.id.passwordEditText)
+    EditText mPasswordEditText;
+    @BindView(R.id.registerTextView)
+    TextView mRegisterTextView;
+    public static final String TAG = LoginActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         ButterKnife.bind(this);
+
         mRegisterTextView.setOnClickListener(this);
-
-
     }
 
     @Override
-    public void onClick(View view) {
-        if (view == mRegisterTextView) {
+    public void onClick(View v) {
+
+        if (v == mRegisterTextView) {
             Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
             startActivity(intent);
             finish();
         }
     }
-
 }
+
+
